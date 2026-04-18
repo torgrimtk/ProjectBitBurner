cec5ba9ce2dead1581f57dcadc9e1b647fb7bbb6
torgrim <147815453+torgrimtk@users.noreply.github.com>
2026-04-18 18:43:58 +0200

    added some functions and made the website responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <h1>Hello!</h1>
+    <BrowserRouter>
+      <h1>Hello!</h1>
+    </BrowserRouter>
   )
 }
 
--- src/api/jsonplaceholder.ts
+++ src/api/jsonplaceholder.ts
@@ -1,1 +1,23 @@
-// this will fetch calls in one place
+import type { User, Post } from "../types";
+
+export async function getUser(id: number): Promise<User> {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    if (!response.ok) {
+        throw new Error(`Network error.`);
+    }
+
+    const userData = await response.json() as User;
+    return userData;
+};
+
+export async function getPosts(): Promise<Post[]> {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+    if (!response.ok) {
+        throw new Error(`Network error.`);
+    }
+
+    const userPost = await response.json() as Post[];
+    return userPost;
+};
